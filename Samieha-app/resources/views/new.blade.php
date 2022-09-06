091973a9ee9f53fa141b1d289a7a2cd93a818805
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-09-07 01:22:30 +0300

    admin dashboard

---
+++ Samieha-app/resources/views/new.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.master')
+
+@section('title')
+  
+  @endsection
+
+  @section('css')
+
+  @endsection
+
+  @section('bar1')
+   
+  @endsection
+
+  @section('bar2')
+   
+  @endsection
+
+
+
+  @section('content')
+   
+  @endsection
+
+@section('scripts')
+
+@endsection
