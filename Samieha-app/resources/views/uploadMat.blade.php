0aea39446a8b54446bfa005f4cd8b7b52708c474
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-09-08 23:03:00 +0300

    .

--- Samieha-app/resources/views/uploadMat.blade.php
+++ Samieha-app/resources/views/uploadMat.blade.php
@@ -17,7 +17,6 @@
   @endsection
 
   @section('content')
-
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
@@ -650,9 +649,6 @@
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
-
-
-
   @endsection
 
   @section('scripts')
@@ -662,8 +658,6 @@
 <script src="{{URL::asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- bs-custom-file-input -->
 <script src="{{URL::asset('assets/plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{URL::asset('assets/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{URL::asset('assets/js/demo.js')}}"></script>
 <!-- Page specific script -->
