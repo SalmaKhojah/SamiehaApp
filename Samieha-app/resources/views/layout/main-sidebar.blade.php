9a13ef1d4fcaa03e0976cca1e3f2427d72de949c
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-09-09 22:28:08 +0300

    .

--- Samieha-app/resources/views/layout/main-sidebar.blade.php
+++ Samieha-app/resources/views/layout/main-sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-warning elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="{{asset('assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
