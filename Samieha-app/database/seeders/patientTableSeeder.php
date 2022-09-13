0c3c9d6cd933cc898db711a8e91cec48c1357abd
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-09-14 00:18:12 +0300

    .

--- Samieha-app/database/seeders/patientTableSeeder.php
+++ Samieha-app/database/seeders/patientTableSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facads\DB;
+use Illuminate\Support\Facades\DB;
+
 
 class patientTableSeeder extends Seeder
 {
