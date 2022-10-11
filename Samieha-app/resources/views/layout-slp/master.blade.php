b6a9cad12ec8f30a4a1eccbdf14b6774db86f0f6
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-10-12 02:46:32 +0300

    .

--- Samieha-app/resources/views/layout-slp/master.blade.php
+++ Samieha-app/resources/views/layout-slp/master.blade.php
@@ -21,26 +21,23 @@
   <!-- Main Sidebar Container -->
   @include('layout-slp.main-sidebar')
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0"></h1>
-          </div>
-          </div> 
-           <!-- /.col -->
+          </div><!-- /.col -->
           <div class="col-sm-10">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="@yield('link1')">@yield('bar1')</a></li>
               <li class="breadcrumb-item active"><a href="@yield('link2')">@yield('bar2')</a></li>
-              
             </ol>
-          </div> -->
-        </div>
-      </div>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
