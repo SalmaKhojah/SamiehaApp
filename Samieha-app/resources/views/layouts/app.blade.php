ac91697fedb40d4f98454c7984208f52f93e03c3
Ghadalamer <94056305+Ghadalamer@users.noreply.github.com>
2022-10-11 09:54:08 +0300

    upload

--- Samieha-app/resources/views/layouts/app.blade.php
+++ Samieha-app/resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                   سميها
+                <img src="{{asset('assets/img/gp logo svg.svg')}}" alt="SamiehaLogo" height="80px" class="mx-auto">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,7 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav me-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
