dff9432cd9ad96a5a0960c49041306a0e2af9377
Ghadalamer <94056305+Ghadalamer@users.noreply.github.com>
2022-10-12 08:42:43 +0300

    Update master.blade.php

--- Samieha-app/resources/views/layout-slp/master.blade.php
+++ Samieha-app/resources/views/layout-slp/master.blade.php
@@ -97,35 +97,6 @@
 
 
 
-
-  @if ($message = Session::get('success'))
-        <div class="alert alert-success alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-    @if ($message = Session::get('error'))
-        <div class="alert alert-danger alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-    @if ($message = Session::get('warning'))
-        <div class="alert alert-warning alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-
-    @if ($message = Session::get('info'))
-        <div class="alert alert-info alert-block">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <p>{{ $message }}</p>
-        </div>
-    @endif
-    
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
