387074065287061ff73e484207908935fc542f6a
salma khojah <98220306+SalmaKhojah@users.noreply.github.com>
2022-11-08 15:29:37 +0300

    .

--- Samieha-app/resources/views/layout/footer.blade.php
+++ Samieha-app/resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
-    <strong>حقوق النشر &copy; 2022-2023 <a href="#">فريق سميها</a>.</strong>
-    كل الحقوق محفوظة.
+    <strong>  &copy; 2022-2023 <a href="#">فريق سميها</a>.</strong>
+
     <div class="float-left d-none d-sm-inline-block">
       <b>النسخة</b> 0.1
     </div>
